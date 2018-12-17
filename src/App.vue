f64b5df1e376efeafa5beb4d9e6f008fb94be235
gowrimanoharir <gowrimanohari.r@gmail.com>
2018-12-16 22:17:02 -0600

    added column sort functionalities

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import '@/assets/main.css';
 import dataTable from './components/dataTable.vue';
 import loader from './components/loader.vue';
 import customizeTable from './data/customizeTable';
+import orderBy from 'lodash.orderby';
 
 export default {
   name: 'app',
@@ -12,23 +13,24 @@ export default {
   },
   data() {
     return {
-      tableData: {},
+      lookUpData: {},
+      tableData: [],
       status: '',
+      currentSort: {
+        fieldKey: 'Date',
+        sortOrder: 'desc'
+      },
     };
   },
   computed: {
     customizeTable() {
       return customizeTable;
     },
-    tableRowKeys() {
-      const { tableData = {} } = this;
-      return Object.keys(tableData) || [];
-    },
     hasData() {
-      return this.status==='resolved' && this.tableRowKeys.length > 0;
+      return this.status==='resolved' && this.tableData.length > 0;
     },
     hasNoData() {
-      return this.status==='resolved' && this.tableRowKeys.length === 0;
+      return this.status==='resolved' && this.tableData.length === 0;
     },
     isLoading() {
       return this.status==='loading';
@@ -39,25 +41,47 @@ export default {
   },
   mounted() {
     this.status = 'loading';
+    const { fieldKey = 'Name', sortOrder = 'asc' } = this.currentSort;
     fetch('/payments').then(response => {
       if(response.status===200) {
         return response.json()
       }
       throw response.statusText;
     }).then(data => {
-      this.tableData = data;
+      this.lookUpData = data;
+      this.tableData = orderBy(data, [fieldKey], [sortOrder]);
       this.status = 'resolved';
     }).catch(() => {
       this.status = 'error';
     });
   },
+  methods: {
+    sortColumn(key) {
+      const { tableData = [], currentSort = {} } = this;
+      const { fieldKey = '', sortOrder = 'asc' } = currentSort;
+      const newSortOrder = fieldKey === key && sortOrder === 'asc' ? 'desc' : 'asc';
+      const sortedData = orderBy(tableData, [key], [newSortOrder]);
+      this.tableData = sortedData;
+      this.currentSort = {
+        fieldKey: key,
+        sortOrder: newSortOrder
+      }
+    }
+  }
 }
 </script>
 
 <template>
   <div id="app" class="mh-36">
     <loader v-if="isLoading" class="mv-24"></loader>
-    <data-table v-if="hasData" class="mv-24" v-bind:tableData="tableData" v-bind:customizeTable="customizeTable" v-bind:tableRowKeys="tableRowKeys"></data-table>
+    <data-table 
+      v-if="hasData" 
+      class="mv-24" 
+      v-bind:tableData="tableData" 
+      v-bind:customizeTable="customizeTable" 
+      v-bind:currentSort="currentSort"
+      v-on:sortColumn="sortColumn">
+    </data-table>
     <div v-if="hasNoData">No payments to display</div>
     <div v-if="isError">There was an error retrieving data, please try later or contact customer service</div>
   </div>
--- src/components/dataTable.vue
+++ src/components/dataTable.vue
@@ -1,38 +1,44 @@
 <script>
+import customizeTable from '../data/customizeTable';
   export default {
     name: 'dataTable',
     props: {
       tableData: {
-        type: Object,
+        type: Array,
         required: true,
-        default: () => ({})
+        default: () => []
       },
       customizeTable: {
         type: Array,
         required: false,
         default: () => []
       },
-      tableRowKeys: {
-        type: Array,
-        required: true,
-        default: () => []
-      },
-      theme: {
-        type: String,
+      currentSort: {
+        type: Object,
         required: false,
-        default: 'base'
+        default: () => ({})
       }
     },
     computed: {
       tableHeader() {
         //if column details are provided use that to create column headers, column style etc
-        const { customizeTable = [], tableData = {}, tableRowKeys = [] } = this;
+        const { customizeTable = [], tableData = [] } = this;
         if(customizeTable.length > 0) {
           return customizeTable.map(item => item.label)
         }
         //if column details are not provided the keys of the data will be applied as header
-        const header = tableRowKeys[0];
-        return Object.keys(tableData[header]);
+        const header = tableData[0];
+        return Object.keys(header);
+      },
+      isAscSort() {
+        return this.currentSort.sortOrder === 'asc';
+      }
+    },
+    methods: {
+      sortColumn(index) {
+        if(customizeTable[index].sortable) {
+          this.$emit('sortColumn', customizeTable[index].key);
+        }        
       }
     }
   }
@@ -43,12 +49,27 @@
     <table class="bds-s bdc-gray bdw-1 w-100">
       <thead>
         <tr>
-          <th v-for="(column, index) in tableHeader" v-bind:key="column" v-bind:class="[{'cur-p-hv': customizeTable[index].sortable}, 'fw-b p-6', customizeTable[index].columnStyle]">{{column}}</th>
+          <td 
+            v-for="(column, index) in tableHeader" 
+            v-bind:key="column" 
+            v-bind:class="['fw-b p-6', customizeTable[index].columnStyle]"
+            v-on:click="sortColumn(index)">
+            <button v-if="customizeTable[index].sortable" v-bind:class="[{'jc-fe': customizeTable[index].columnStyle === 'ta-r'},'ai-c cur-p-hv d-f w-100']">
+              {{column}}
+              <svg v-if="customizeTable[index].key === currentSort.fieldKey && isAscSort" viewBox="0 0 24 24" class="h-19px w-19px">
+                <path d="m 6.5,17.5 14,0 -7,-11 z"></path>
+              </svg>
+              <svg v-if="customizeTable[index].key === currentSort.fieldKey && !isAscSort" viewBox="0 0 24 24" class="h-19px w-19px">
+                <path d="m 20.5,6.5 -14,0 7,11 z"></path>
+              </svg>
+            </button>
+            <div v-else>{{column}}</div>
+          </td> 
         </tr>
       </thead>
       <tbody>
-        <tr v-for="rowKey in tableRowKeys" v-bind:key="rowKey" class="bdts-s bdc-gray bdw-1">
-          <td v-for="(item,index) in tableHeader" v-bind:key="`${rowKey}-col${index}`" v-bind:class="['p-6', customizeTable[index].columnStyle]">{{tableData[rowKey][item] || '-'}}</td>
+        <tr v-for="(item, rowIndex) in tableData" v-bind:key="item.ID" class="bdts-s bdc-gray bdw-1">
+          <td v-for="(item,index) in tableHeader" v-bind:key="`${item.ID}-col${index}`" v-bind:class="['p-6', customizeTable[index].columnStyle]">{{tableData[rowIndex][item] || '-'}}</td>
         </tr>
       </tbody>
     </table>
